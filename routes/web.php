c36a216cde8ec1329572721379acc7062601f3b4
irwan-g10 <irwangumilar111@gmail.com>
2025-04-13 11:05:20 +0700

    upload completed project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,73 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('task.index');
+});
+
+Route::get('/tasks', function ()  {
+    return view('index', [
+        'tasks' => Task::latest()->paginate(10),
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task)  {
+
+    return view('edit', [
+        'task' => $task,
+    ]);
+})->name('tasks.edit');
+Route::get('/tasks/{task}', function (Task $task)  {
+
+    return view('show', [
+        'task' => $task,
+    ]);
+})->name('tasks.show');
+
+Route::post('/tasks', function (TaskRequest $request)  {
+
+
+    $task = Task::create($request->validated());
+
+    return redirect()->route('tasks.show', [ 'task' => $task->id ])
+        ->with('success', 'Task created successfully');
+
+})->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request)  {
+
+
+    // $task->save();
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', [ 'task' => $task->id ])
+        ->with('success', 'Task updated successfully');
+
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task)  {
+
+    $task->delete();
+
+    return redirect()->route('task.index')
+        ->with('success', 'Task deleted successfully');
+
+})->name('tasks.destroy');
+
+Route::put('/tasks/{task}/toggle-complete', function (Task $task ){
+    $task->toggleComplete();
+
+    return redirect()->back()->with('success', 'Task updated successfully');
+})->name('tasks.toggle-complete');
+
+Route::fallback(function () {
+    return 'Still get somewhere';
 });
